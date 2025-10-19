4288164f3159c216b77de3110c9d8bc877a8619b
Sp00nzzz <mike.nguyen992004@gmail.com>
2025-10-19 14:26:47 -0400

    vercel update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/queens-party/', // 👈 this line is crucial
+  // base: '/queens-party/', // 👈 removed for Vercel deployment
 })
